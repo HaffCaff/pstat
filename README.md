7ce16437b2a65bafe26de38129cf396a5d2dee7d
Anthony E. <AnthonyDEpps@gmail.com>
2024-02-08 23:20:51 -0500

    pstat v0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # pstat
+
+`pstat` is a simple cli tool that will iterate through a directory and let you know which project are stale, and which projects are active. This is determined by directory mod time (date last modified). Currently, if it the project mod date is more than 30 days old, it will be labeled as stale.
+
+## Future Improvements:
+
+- Allow customization - Allow user to change modTime date threshold
+- Prompt to stale projects to new `stale` directory within current dir
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	// Create slices for active and stale projects
+	active := []string{}
+	stale := []string{}
+
+	// Get the root directory
+	root, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Open the root directory
+	dir, err := os.Open(root)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dir.Close()
+
+	// Read the contents of the root directory
+	entries, err := dir.Readdir(-1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Calculate the threshold date (30 days ago)
+	thresholdDate := time.Now().AddDate(0, 0, -30)
+
+	// Iterate over the directory entries
+	for _, entry := range entries {
+		// Check if the entry is a directory
+		if entry.IsDir() {
+			// Get the modification time of the directory
+			modTime := entry.ModTime()
+
+			// Check if the modification time is before the threshold date
+			if modTime.Before(thresholdDate) {
+				stale = append(stale, entry.Name())
+			} else {
+				active = append(active, entry.Name())
+			}
+		}
+	}
+
+	// Print the results
+	// actives := fmt.Sprintf("Active projects: %s", active)
+	act := strings.Join(active, ", ")
+	actives := fmt.Sprintf("Active projects: %s", act)
+	stl := strings.Join(stale, ", ")
+	stales := fmt.Sprintf("Stale projects: %s", stl)
+	color.Green(actives)
+	color.Yellow(stales)
+}
